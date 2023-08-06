94a5330250178c768004d8aa4a38a90eef85dc7e
Unais k <unais5676@gmail.com>
2023-08-06 21:21:42 +0530

    added delete option to delete the todo list when clicking delete button

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -6,21 +6,26 @@ uuidv4();
 
 export const TodoWrapper = () => {
     const [todos,setTodos]= useState([])
-
+    //for adding the todo activities when submitting form
     const addTodo = (todo)=>{
         setTodos([...todos,{id : uuidv4(), task: todo, completed : false, isEditing:false}])
         console.log(todos)
     }
-
+    // for mark as todo list completed and viseversa by clicking todo list activity
     const toggleComplete = id => {
         setTodos(todos.map(todo=> todo.id === id?{...todo,completed:!todo.completed}:todo))
     }
+
+    // for deleting todo list from the todos state by clicking delete icon
+    const deleteTodo = id =>{
+        setTodos(todos.filter(todo => todo.id !== id))
+    }   
   return (
     <div className='TodoWrapper'>
         <h1>Get Things Done!</h1>
         <TodoForm addTodo={addTodo} />
         {todos.map((todo,index)=>(
-              <Todo task = {todo}  key = {index} toggleComplete = {toggleComplete} />
+              <Todo task = {todo}  key = {index} toggleComplete = {toggleComplete} deleteTodo = {deleteTodo}/>
         ))}
       
     </div>
