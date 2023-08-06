ac4c1217ab19edea0e90c680ea925fc5d288ec93
Unais k <unais5676@gmail.com>
2023-08-06 20:51:07 +0530

    added strings to todos state array and sidplay in the todo list

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { TodoForm } from './TodoForm'
+import {v4 as uuidv4} from 'uuid'
+import { Todo } from './Todo';
+uuidv4();
 
 export const TodoWrapper = () => {
+    const [todos,setTodos]= useState([])
+
+    const addTodo = (todo)=>{
+        setTodos([...todos,{id : uuidv4(), task: todo, completed : false, isEditing:false}])
+        console.log(todos)
+    }
   return (
-    <div>TodoWrapper</div>
+    <div className='TodoWrapper'>
+        <h1>Get Things Done!</h1>
+        <TodoForm addTodo={addTodo} />
+        {todos.map((todo,index)=>(
+              <Todo task = {todo}  key = {index} />
+        ))}
+      
+    </div>
   )
 }
