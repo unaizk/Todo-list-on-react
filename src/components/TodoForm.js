4a5a5766b65db2a5c8afe6e9a1050fb7d3d04e8e
Unais k <unais5676@gmail.com>
2023-08-08 15:21:25 +0530

    added validation

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,15 +2,26 @@ import React,{useState} from 'react'
 
 export const TodoForm = ({addTodo}) => {
     const [value,setValue]= useState("");
+    const [err,setErr]= useState("")
     const handleSubmit = e=>{
         e.preventDefault();
-        addTodo(value)
-        setValue("")
+
+        if (!value.trim()) {
+          setErr('Please add Items....!')
+          
+          return;
+        }
+        
+          addTodo(value)
+          setValue("")
     }
   return (
     <form className='TodoForm' onSubmit={handleSubmit}>
-        <input type='text' className='todo-input' value = {value} placeholder='What is the task today?' onChange={(e)=>setValue(e.target.value)}/>
+        <input type='text' className='todo-input' value = {value} placeholder='What is the task today?' 
+        onChange={(e)=>{setValue(e.target.value);
+        setErr('')}}/>
         <button type='submit' className='todo-btn'>Add Task</button>
+        {err?<p style={{color:"red"}}>{err}</p>:""}
     </form>
   )
 }
